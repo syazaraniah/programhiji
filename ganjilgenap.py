5f3252316a07099e6d3bfd09beec5517c5a4d4ef
SyazaRaniah <ranireynaza20@gmail.com>
2026-01-19 03:08:31 +0000

    versi 1.1 - ganjilgenap

---
+++ ganjilgenap.py
@@ -0,0 +1,8 @@
+bil = int(input("Masukkan bilangan:"))
+
+if bil % 2 == 0:
+    print (bil,"adalah GENAP")
+else:
+    print (bil,"adalah GANJIL")
+
+print("program ini saya buat melalui codespace")
